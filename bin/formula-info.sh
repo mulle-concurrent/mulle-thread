8906b473cbd6207f51551d230f7ae732aade75ab
Nat! <nat@mulle-kybernetik.com>
2017-06-14 15:37:44 +0200

    Use templates to create .md files where necessary

--- bin/formula-info.sh
+++ bin/formula-info.sh
@@ -1,3 +1,3 @@
-PROJECT="MulleThread"   # your project name, requires camel-case
+PROJECT="mulle-thread"   # your project name, requires camel-case
 DESC="Cross-platform threads and atomic operations"
 DEPENDENCIES='${DEPENDENCY_TAP}mulle-c11'
